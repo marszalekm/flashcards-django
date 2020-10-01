c30ddbf769a4c8e15d7a84c0bf4ff12d3c69393a
Michał <mictommarszalek@gmail.com>
2020-10-01 22:21:31 +0200

    Models rearrangement for multiple desks

--- cards/admin.py
+++ cards/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from .models import Word
+from .models import Word, Pack
 # Register your models here.
 
-admin.site.register(Word)
+class WordProfile(admin.ModelAdmin):
+
+    list_display = ['original', 'translation', 'deck']
+
+
+class PackProfile(admin.ModelAdmin):
+
+    list_display = ['name', 'description', 'author']
+
+admin.site.register(Word, WordProfile)
+
+admin.site.register(Pack, PackProfile)
--- cards/models.py
+++ cards/models.py
@@ -2,19 +2,29 @@ from django.db import models
 from django.conf import settings
 
 
+class Pack(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = ("Deck name")
+
+    def __str__(self):
+        return self.name
+
+
 class Word(models.Model):
     original = models.CharField(max_length=50)
     translation = models.CharField(max_length=50)
-    deck = models.CharField(max_length=20)
+    deck = models.ForeignKey(Pack, default="Deleted deck", on_delete=models.SET_DEFAULT)
+
     # owner = models.ForeignKey('auth.User', related_name='cards', on_delete=models.CASCADE)
     # highlighted = models.TextField()
+
+    class Meta:
+        verbose_name = ("Word")
 
     def __str__(self):
         return self.original
 
-# class Pack(models.Model):
-#     author = models.ForeignKey(
-#         settings.AUTH_USER_MODEL,
-#         on_delete=models.CASCADE,
-#     )
-#     deck = models.CharField(max_length=20)
